68604e8ef28a4facde10cdba8ceb63049b256bc3
August Tollerup <augusttollerup@augusts-mbp.lan>
2024-04-22 13:10:42 +0200

    Feat: PDF Parser function

---
+++ pdf_parser.py
@@ -0,0 +1,19 @@
+from pypdf import PdfReader
+from pathlib import Path
+from pprint import pprint
+
+def parse_pdf(pdf_path):
+    pdf_reader = PdfReader(pdf_path)
+    pages = pdf_reader.pages
+
+    full_text = ''
+    for page in pages:
+        full_text += page.extract_text()
+
+    return full_text
+
+if __name__ == '__main__':
+    pdf_path = Path('sample.pdf')
+    full_text = parse_pdf(pdf_path)
+    print(full_text)
+   
